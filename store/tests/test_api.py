fd2f1a971f1b73d2a0a389cc49ef2ea3dd551350
Vadzim <workvadzim@gmail.com>
2024-01-24 23:40:39 +0300

    Lesson #5 CRUD REST Framework
    * Installed postman, made basic queries via sessionid
    - Created a new book via POST request to postman
    - Edited new book via PUT request to postman
    - New book deleted via DELETE request to postman
    * In store views in permission_classes variable added access to data for all users
    * In store test in test_serializers rewritten test
    * In store test in test_api added tests test_create, test_update, test_delete

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,12 +1,16 @@
+import json
+
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from store.models import Book
 from store.serializers import BookSerializer
+from django.contrib.auth.models import User
 
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):  # func is run every time before
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Test book 1', price=25,
                                           author_name='Author 1')
         self.book_2 = Book.objects.create(name='Test book 2', price=55,
@@ -39,8 +43,59 @@ class BooksApiTestCase(APITestCase):
 
     def test_get_myself(self):
         url = reverse('book-list')
+        # This line is getting the URL for the book list view.
+        # The `reverse` function is used to get the URL from the URL pattern name.
         response = self.client.get(url, data={'name': 'Test book 2'})
         # get coverage for name and variable setUp value from
         serializer_data = BookSerializer([self.book_2], many=True).data
+        #  This line is serializing the book instance `self.book_2` using the
+        #  `BookSerializer` and getting the serialized data.
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+        #  This line is checking if the response status code is HTTP 200 OK.
         self.assertEqual(serializer_data, response.data)
+        #  This line is checking if the serialized data matches the response data.
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+                "id": 5,
+                "name": "Essence 5",
+                "price": "150.00",
+                "author_name": "Author 5"
+                }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+                "name": self.book_1.name,
+                "price": 575,
+                "author_name": self.book_1.author_name
+                }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        # self.book_1 = Book.objects.get(id=self.book_1.id) or:
+        self.book_1.refresh_from_db()
+        self.assertEqual(575, self.book_1.price)
+
+    def test_delete_myself(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.delete(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(2, Book.objects.all().count())
+
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 import django_filters
 from django.shortcuts import render
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from store.models import Book
 from store.serializers import BookSerializer
@@ -11,7 +11,7 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]  # permission to get data for all users
     filterset_fields = ['name', 'price']   # filterset_fields in new versions instead of filter_fields
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
